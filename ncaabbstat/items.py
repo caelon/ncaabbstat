ed1b5530b24ffe322d0a286f2154d7c0a9a93a0c
Don VanDemark <caelon@gmail.com>
2013-02-09 16:04:33 -0500

    Lists all stats in single line.

--- ncaabbstat/items.py
+++ ncaabbstat/items.py
@@ -16,8 +16,6 @@ class DailylinksItem(Item):
         return "%s at %s - %s" % (self.get('visitor'), self.get('home'), self.get('boxscorelink'))
 
 class PlayerstatItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
     name = Field()
     position = Field()
     atbats = Field()
@@ -29,6 +27,20 @@ class PlayerstatItem(Item):
     putouts = Field()
     assists = Field()
     leftonbase = Field()
+    inningsp = Field()
+    hitsp = Field()
+    runsp = Field()
+    earnedrunsp = Field()
+    walksp = Field()
+    strikeoutsp = Field()
+    wildpitchesp = Field()
+    balksp = Field()
+    hbp = Field()
+    ibbp = Field()
+    atbatsp = Field()
+    battersfacedp = Field()
+    flyoutsp = Field()
+    groundoutsp = Field()
 
     def __str__(self):
-        return "%s %s went %i for %i with %i runs, %i RBI, %i walks, %i strikeouts and left %i runners on base. He had %i putouts and %i assists." % (self.get('position'), self.get('name'), self.get('hits'), self.get('atbats'), self.get('runs'), self.get('rbi'), self.get('basesonballs'), self.get('strikeouts'), self.get('leftonbase'), self.get('putouts'), self.get('assists'))
+        return "%s %s went %i for %i with %i runs, %i RBI, %i walks, %i strikeouts and left %i runners on base. He had %i putouts and %i assists. He pitched %f innings. He gave up %i hits and %i runs with %i earned. He had %i walks (%i intentional), %i stikeouts, %i wild pitches, %i balks and hit %i batters. There were %i at-bats and he faced %i batters. He forced %i flyouts and %i groundouts." % (self.get('position'), self.get('name'), self.get('hits'), self.get('atbats'), self.get('runs'), self.get('rbi'), self.get('basesonballs'), self.get('strikeouts'), self.get('leftonbase'), self.get('putouts'), self.get('assists'), self.get('inningsp'), self.get('hitsp'), self.get('runsp'), self.get('earnedrunsp'), self.get('walksp'), self.get('ibbp'), self.get('strikeoutsp'), self.get('wildpitchesp'), self.get('balksp'), self.get('hbp'), self.get('atbatsp'), self.get('battersfacedp'), self.get('flyoutsp'), self.get('groundoutsp'))
--- ncaabbstat/spiders/d1bb_spider.py
+++ ncaabbstat/spiders/d1bb_spider.py
@@ -1,7 +1,7 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 
-from ncaabbstat.items import DailylinksItem
+from ncaabbstat.items import DailylinksItem, PlayerstatItem
 
 class D1BBSpider(BaseSpider):
     name = "D1BB"
@@ -32,5 +32,96 @@ class D1BBSpider(BaseSpider):
 
         return items
 
-# gets all team names: hxs.select('//td[descendant::a[contains(., "Box Score")]]/preceding-sibling::td//a/text()').extract()
+class BoxscoreStyle1Spider(BaseSpider):
+    name = "bx1"
+    allowed_domains = ["lsusports.com"]
+    start_urls = [
+        "http://www.lsusports.net/fls/5200/assets/docs/bb/12stats/lsu407.htm"
+    ]
 
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        lines = hxs.select('//tr[@bgcolor="#ffffff"]')
+        items = []
+
+        for line in lines:
+            if str(line.select('td[11]').extract()) == "[]":
+                namepos = str(line.select('td[1]/font/text()').extract())
+                namepos = namepos[3:-6]
+                namepos = namepos.replace("\\xa0","")
+                if namepos != "":
+                    if namepos[:1] == " ":
+                        namepos = namepos[1:]
+                    if namepos[:6] != "Totals":
+                        item = PlayerstatItem()
+                        if namepos[-1:] == 'p' or namepos[-1:] == 'c':
+                            item['position'] = namepos[-1:]
+                            item['name'] = namepos[:-2]
+                        else:
+                            item['position'] = namepos[-2:]
+                            item['name'] = namepos[:-3]
+                        atbats = str(line.select('td[2]/font/text()').extract())
+                        item['atbats'] = int(atbats[3:-6])
+                        runs = str(line.select('td[3]/font/text()').extract())
+                        item['runs'] = int(runs[3:-6])
+                        hits = str(line.select('td[4]/font/text()').extract())
+                        item['hits'] = int(hits[3:-6])
+                        rbi = str(line.select('td[5]/font/text()').extract())
+                        item['rbi'] = int(rbi[3:-6])
+                        basesonballs = str(line.select('td[6]/font/text()').extract())
+                        item['basesonballs'] = int(basesonballs[3:-6])
+                        strikeouts = str(line.select('td[7]/font/text()').extract())
+                        item['strikeouts'] = int(strikeouts[3:-6])
+                        putouts = str(line.select('td[8]/font/text()').extract())
+                        item['putouts'] = int(putouts[3:-6])
+                        assists = str(line.select('td[9]/font/text()').extract())
+                        item['assists'] = int(assists[3:-6])
+                        leftonbase = str(line.select('td[10]/font/text()').extract())
+                        item['leftonbase'] = int(leftonbase[3:-6])
+                        item['inningsp'] = item['hitsp'] = item['runsp'] = item['earnedrunsp'] = item['walksp'] = item['strikeoutsp'] = item['wildpitchesp'] = item['balksp'] = item['hbp'] = item['ibbp'] = item['atbatsp'] = item['battersfacedp'] = item['flyoutsp'] = item['groundoutsp'] = 0
+#                        print position, name, atbats, hits, runs, rbi, basesonballs, strikeouts, putouts, assists, leftonbase
+                        items.append(item)
+            elif str(line.select('td[11]').extract()) != "[]":
+                namepos = str(line.select('td[1]/font/text()').extract())
+                namepos = namepos[3:-6]
+                namepos = namepos.replace("\\xa0","")
+                if namepos != "":
+                    if namepos[:1] == " ":
+                        namepos = namepos[1:]
+                    if namepos[:6] != "Totals":
+                        item = PlayerstatItem()
+                        item['position'] = 'p'
+                        item['name'] = namepos
+                        ips = str(line.select('td[2]/font/text()').extract())
+                        item['inningsp'] = float(ips[3:-6])
+                        hitsp = str(line.select('td[3]/font/text()').extract())
+                        item['hitsp'] = int(hitsp[3:-6])
+                        runsp = str(line.select('td[4]/font/text()').extract())
+                        item['runsp'] = int(runsp[3:-6])
+                        earnedrunsp = str(line.select('td[5]/font/text()').extract())
+                        item['earnedrunsp'] = int(earnedrunsp[3:-6])
+                        walksp = str(line.select('td[6]/font/text()').extract())
+                        item['walksp'] = int(walksp[3:-6])
+                        strikeoutsp = str(line.select('td[7]/font/text()').extract())
+                        item['strikeoutsp'] = int(strikeoutsp[3:-6])
+                        wildpitchesp = str(line.select('td[8]/font/text()').extract())
+                        item['wildpitchesp'] = int(wildpitchesp[3:-6])
+                        balksp = str(line.select('td[9]/font/text()').extract())
+                        item['balksp'] = int(balksp[3:-6])
+                        hbp = str(line.select('td[10]/font/text()').extract())
+                        item['hbp'] = int(hbp[3:-6])
+                        ibbp = str(line.select('td[11]/font/text()').extract())
+                        item['ibbp'] = int(ibbp[3:-6])
+                        atbatsp = str(line.select('td[12]/font/text()').extract())
+                        item['atbatsp'] = int(atbatsp[3:-6])
+                        battersfacedp = str(line.select('td[13]/font/text()').extract())
+                        item['battersfacedp'] = int(battersfacedp[3:-6])
+                        flyoutsp = str(line.select('td[14]/font/text()').extract())
+                        item['flyoutsp'] = int(flyoutsp[3:-6])
+                        groundoutsp = str(line.select('td[15]/font/text()').extract())
+                        item['groundoutsp'] = int(groundoutsp[3:-6])
+                        item['atbats'] = item['hits'] = item['runs'] = item['rbi'] = item['basesonballs'] = item['strikeouts'] = item['putouts'] = item['assists'] = item['leftonbase'] = 0
+#                        print position, name, atbats, hits, runs, rbi, basesonballs, strikeouts, putouts, assists, leftonbase
+                        items.append(item)
+
+        return items
